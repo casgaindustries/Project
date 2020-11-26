bd5e60ea1cce8d4acaa7fe128db6de6f30623932
Tim <timheid97@gmail.com>
2020-11-27 00:36:39 +0100

    Encryption, decryption and keygeneration for messages. Contains demo, delete after viewing

--- Encryption.py
+++ Encryption.py
@@ -13,7 +13,7 @@ def encrypt_message(plain_text, pubkey):
     # Encrypts message to RSA cipher using the public key of the recipient
     # rsa.encrypt takes UTF-8 encoding only
     plain_text = plain_text.encode('utf8')
-    cipher_ = rsa.encrypt(plain_text, pubkey)
+    cipher = rsa.encrypt(plain_text, pubkey)
     return cipher
 
 
